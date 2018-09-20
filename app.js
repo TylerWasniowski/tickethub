b06d8ee7fa2917416ca1c86fa3dc93da3a7e7a70
Zan Javed <zan@Zans-MBP.attlocal.net>
2018-09-19 22:27:06 -0700

    added basic home page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ticketsRouter = require('./routes/tickets');
+var homeRouter = require('./routes/home');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tickets', ticketsRouter);
+app.use('/home', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
