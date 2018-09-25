af19017f0967c2ff156cd8ea3dafa1ba3e59d2e3
Justin Ocampo <lingling287@gmail.com>
2018-09-25 14:09:14 -0700

    login functionality

--- routes/createaccount.js
+++ routes/createaccount.js
@@ -5,33 +5,36 @@ var mongo = require('mongodb');
 
 var url = 'mongodb://localhost:27017/tickethub';
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
     res.render('createaccount', {});
 });
 
 
 router.post('/submit', function(req, res, next) {
+    var name = req.body.name;
     var username = req.body.username;
     var email = req.body.email;
     var password = req.body.password;
 
 
     var item = {
+        name: name,
         username: username,
         email: email,
         password: password
     };
 
+    console.log(name + ' ' + username + ' ' + email + ' ' + password);
+
     mongo.connect(url, function(err, db) {
         db.collection('user-data').insertOne(item, function(err, result) {
-            //assert.equal(null, err);
             console.log('Item inserted');
             db.close();
         });
     });
 
-    res.render('register', {username: username, email: email, password: password});
+    res.render('register', {name: name, username: username, email: email, password: password});
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+
+router.get('/account',function (req, res, next) {
+  res.render('account', {name : req.session.name, username : req.session.username, email : req.session.email, success : req.session.success,})
+})
+
 module.exports = router;
