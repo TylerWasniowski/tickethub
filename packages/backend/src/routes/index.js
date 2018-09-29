d6d97a0c2b037f33e7d0447648fb4b3d0bee1081
TylerWasniowski <TylerWasniowski@gmail.com>
2018-09-29 14:05:44 -0400

    Removed already migrated views

--- packages/backend/src/routes/index.js
+++ packages/backend/src/routes/index.js
@@ -1,62 +1,54 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+import mongodb from 'mongodb';
 
-var mongo = require('mongodb');
-var url = 'mongodb://localhost:27017/tickethub';
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+const url = 'mongodbdb://localhost:27017/tickethub';
 
 // TODO: Add to frontend
 // router.get('/account',function (req, res, next) {
 //   res.render('account', {name : req.session.name, username : req.session.username, email : req.session.email, success : req.session.success,})
 // })
 
-// router.get('/account/update',function (req, res, next) {
-//     res.render('updateaccount', {});
-// })
-
-
 // TODO: Move to it's own account routes file
-router.post('/account/submit', function (req, res, next) {
+router.post('/account/submit', (req, res, next) => {
+  const item = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
 
-    var item = {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    };
+  req.session.name = item.name;
+  req.session.email = item.email;
+  req.session.password = item.password;
 
+  console.log(item);
 
-    req.session.name = item.name;
-    req.session.email = item.email;
-    req.session.password = item.password;
-
-    console.log(item);
-
-
-    mongo.connect(url, function(err, db) {
-        var dbo = db.db('tickethub');
-        dbo.collection('user-data').updateOne({"username": req.session.username}, {$set: item}, function(err, result) {
-            if (err) throw err;
+  mongodb.connect(
+    url,
+    (err, db) => {
+      const dbo = db.db('tickethub');
+      dbo
+        .collection('user-data')
+        .updateOne(
+          { username: req.session.username },
+          { $set: item },
+          (dbErr, result) => {
+            if (dbErr) throw dbErr;
             console.log('Item updated');
             db.close();
+          }
+        );
+    }
+  );
 
-        });
-    });
-
-
-    res.redirect('/account');
-
-
+  res.redirect('/account');
 });
 
 // TODO: Move to other route file
-router.get('/logout', function (req, res, next) {
-    req.session.destroy();
-    res.redirect('login');
+router.get('/logout', (req, res, next) => {
+  req.session.destroy();
+  res.redirect('login');
 });
 
-module.exports = router;
+export default router;
