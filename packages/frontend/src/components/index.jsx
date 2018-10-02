6528811532c362ba2aff67b6693d4aa8738c8270
TylerWasniowski <TylerWasniowski@gmail.com>
2018-10-01 23:20:12 -0700

    Created Sidebar component

--- packages/frontend/src/components/index.jsx
+++ packages/frontend/src/components/index.jsx
@@ -6,21 +6,26 @@ import { hot } from 'react-hot-loader';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 // import routes from '../routes';
 
-import Home from './home.jsx';
-import Login from './login.jsx';
-import CreateAccount from './create-account.jsx';
-import UpdateAccount from './update-account.jsx';
+import Home from './home';
+import Login from './login';
+import CreateAccount from './create-account';
+import UpdateAccount from './update-account';
 
 type Props = {
   title: string,
 };
 
 const App = ({ title }: Props): Node => (
+  <div>
+    {title}
+
+    <Sidebar />
+  </div>
+);
+
+const Sidebar = (): Node => (
   <Router>
     <div>
-      {title}
-
-      {/* TODO: Make into sidebar react component */}
       <li>
         <Link to="/">Home</Link>
       </li>
@@ -34,9 +39,6 @@ const App = ({ title }: Props): Node => (
         <Link to="/update-account">Update Account</Link>
       </li>
 
-      {/* <Router /> */}
-
-      {/* TODO: Make into react component */}
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/create-account" component={CreateAccount} />
