fbd0eb0e8437fb36e1965037d729f0fa22d45c3c
TylerWasniowski <TylerWasniowski@gmail.com>
2018-10-04 15:06:46 -0700

    Renamed routes and changed 'String' to 'string'

--- packages/frontend/src/components/routes.jsx
+++
@@ -1,6 +0,0 @@
-// @flow
-
-export const HomeRoute: String = '/';
-export const LoginRoute: String = '/login';
-export const CreateAccountRoute: String = '/create-account';
-export const UpdateAccountRoute: String = '/update-account';
