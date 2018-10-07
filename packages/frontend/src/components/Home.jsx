c6825dbcfe5e4ee3b845185b82b1e8e8d3ec4290
TylerWasniowski <TylerWasniowski@gmail.com>
2018-10-06 22:09:11 -0700

    Added searchbar

--- packages/frontend/src/components/Home.jsx
+++ packages/frontend/src/components/Home.jsx
@@ -1,7 +1,33 @@
 // @flow
+import '../styles/home.css';
 import React from 'react';
 import type { Node } from 'react';
 
-const Home = (): Node => <h1>HOMEPAGE</h1>;
+class Home extends React.Component {
+  state: {
+    querry: string,
+    suggestions: Array<string>,
+  } = {
+    query: '',
+    suggestions: [],
+  };
+
+  render() {
+    const { query, suggestions } = this.state;
+
+    return (
+      <div className="search-container">
+        <div className="searchbar-container">
+          <input className="searchbar" placeholder="Search TicketHub" />
+        </div>
+        <ul className="search-suggestions">
+          {suggestions.map(suggestion => (
+            <li>{suggestion}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Home;
