f84ce91cd662b72be45e74fa6e39ec7e542c28b0
TylerWasniowski <TylerWasniowski@gmail.com>
2018-10-08 10:58:56 -0700

    Implemented query updating on input change

--- packages/frontend/src/components/Home.jsx
+++ packages/frontend/src/components/Home.jsx
@@ -3,16 +3,28 @@ import '../styles/home.css';
 import React from 'react';
 
 import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 import SearchIcon from '@material-ui/icons/Search';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleQueryChange = this.handleQueryChange.bind(this);
+  }
+
   state: {
-    querry: string,
+    query: string,
     suggestions: Array<string>,
   } = {
     query: '',
     suggestions: [],
   };
+
+  handleQueryChange(event) {
+    this.setState({ query: event.target.value });
+  }
 
   render() {
     const { query, suggestions } = this.state;
@@ -20,16 +32,21 @@ class Home extends React.Component {
     return (
       <div className="search-container">
         <div className="searchbar-container">
-          <input className="searchbar" placeholder="Search TicketHub" />
+          <input
+            className="searchbar"
+            placeholder="Search TicketHub"
+            onChange={this.handleQueryChange}
+          />
           <IconButton className="search-button">
             <SearchIcon />
           </IconButton>
         </div>
-        <ul className="search-suggestions">
+        <List className="search-suggestions">
           {suggestions.map(suggestion => (
-            <li>{suggestion}</li>
+            <ListItem className="search-suggestion">{suggestion}</ListItem>
           ))}
-        </ul>
+          <ListItem className="search-suggestion">{query}</ListItem>
+        </List>
       </div>
     );
   }
