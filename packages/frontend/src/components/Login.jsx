b5c14ff5fece8a0e218a50af65f745f2d2ba8f47
TylerWasniowski <TylerWasniowski@gmail.com>
2018-10-14 15:20:48 -0700

    Implemented material ui for login

--- packages/frontend/src/components/Login.jsx
+++ packages/frontend/src/components/Login.jsx
@@ -1,25 +1,125 @@
 // @flow
+import '../styles/login.css';
 import React from 'react';
 import type { Node } from 'react';
 
-import BoxContainer from './BoxContainer';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-const Login = (): Node => (
-  <BoxContainer>
-    <h1>Login</h1>
-    <p>Login</p>
-    <form action="login/submit" method="post">
-      <br />
-      <p>
-        Username <input type="text" name="username" />
-      </p>
-      <p>
-        Password <input type="password" name="password" />
-      </p>
-      <button type="submit">Submit</button>
-      <br />
-    </form>
-  </BoxContainer>
-);
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  state: {
+    username: string,
+    password: string,
+  } = {
+    username: '',
+    password: '',
+  };
+
+  handleUsernameChange(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { username, password } = this.state;
+
+    console.log(event);
+
+    fetch('/login/submit', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+    })
+      .then(response => response.json())
+      .then(response => JSON.stringify(response))
+      .then(response => alert(response))
+      .catch(err => console.log(err));
+  }
+
+  render(): Node {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <Paper className="login-container">
+          <h1 className="login-title">Login</h1>
+          <form onSubmit={this.handleSubmit}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="username">Username</InputLabel>
+              <Input
+                id="username"
+                name="username"
+                autoComplete="username"
+                onChange={this.handleUsernameChange}
+                autoFocus
+              />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={this.handlePasswordChange}
+              />
+            </FormControl>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className="submit-button"
+            >
+              Login
+            </Button>
+          </form>
+        </Paper>
+      </React.Fragment>
+    );
+  }
+}
+
+// const Login = (): Node => (
+//   <BoxContainer>
+//     <h1>Login</h1>
+//     <p>Login</p>
+//     <form action="login/submit" method="post">
+//       <br />
+//       <p>
+//         Username <input type="text" name="username" />
+//       </p>
+//       <p>
+//         Password <input type="password" name="password" />
+//       </p>
+//       <button type="submit">Submit</button>
+//       <br />
+//     </form>
+//   </BoxContainer>
+// );
 
 export default Login;
