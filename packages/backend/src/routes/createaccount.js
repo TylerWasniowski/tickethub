f64a8e665667a8798ea6d550d770138dd308495f
Zan Javed <zjaved101@gmail.com>
2018-10-18 23:14:27 -0700

    refactored db connection code

--- packages/backend/src/routes/createaccount.js
+++ packages/backend/src/routes/createaccount.js
@@ -1,15 +1,15 @@
 import mysql from 'mysql';
-
 import express from 'express';
+import db from '../lib/database';
 
 const router = express.Router();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
 
 // TODOD: Move to frontend
 router.get('/', (req, res, next) => {
@@ -24,14 +24,10 @@ router.post('/submit', (req, res, next) => {
     password: req.body.password,
   };
 
-  connection.query('INSERT INTO users SET ?', ret, (error, results, fields) => {
-    // connection.query('SELECT * FROM users WHERE name = ?', ret.name, function (error, results, fields) {
+  db.query('INSERT INTO users SET ?', ret, (error, results, fields) => {
     if (error) throw error;
-    // res.send(query.sql);
     res.redirect('/');
   });
-  // connection.end();
-  // res.json(ret);
 });
 
 export default router;
