a576977231e0b77196072cc98ae748688c327ab6
Zan Javed <zjaved101@gmail.com>
2018-10-21 22:09:53 -0700

    added password hashing

--- packages/backend/src/routes/createaccount.js
+++ packages/backend/src/routes/createaccount.js
@@ -1,5 +1,6 @@
-import mysql from 'mysql';
+// import mysql from 'mysql';
 import express from 'express';
+import bcrypt from 'bcrypt';
 import db from '../lib/database';
 
 const router = express.Router();
@@ -17,17 +18,37 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/submit', (req, res, next) => {
-  const ret = {
-    name: req.body.name,
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  };
+  // const ret = {
+  //   name: req.body.name,
+  //   username: req.body.username,
+  //   email: req.body.email,
+  //   password: req.body.password,
+  // };
 
-  db.query('INSERT INTO users SET ?', ret, (error, results, fields) => {
-    if (error) throw error;
-    res.redirect('/');
-  });
+  bcrypt.hash(
+    req.body.password,
+    parseInt(process.env.BCRYPT_SALT, 10),
+    (hashErr, hash) => {
+      if (hashErr) throw hashErr;
+
+      const ret = {
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+        password: hash,
+      };
+
+      db.query('INSERT INTO users SET ?', ret, (err, results, fields) => {
+        if (err) throw err;
+        res.redirect('/');
+      });
+    }
+  );
+
+  // db.query('INSERT INTO users SET ?', ret, (error, results, fields) => {
+  //   if (error) throw error;
+  //   res.redirect('/');
+  // });
 });
 
 export default router;
