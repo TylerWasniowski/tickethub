13624f26b71aff6f7d2c141bf6d36986038a6adc
Zan Javed <zjaved101@gmail.com>
2018-10-22 14:10:11 -0700

    compares hashed passw with user provided passw

--- packages/backend/src/routes/login.js
+++ packages/backend/src/routes/login.js
@@ -1,15 +1,17 @@
 import mysql from 'mysql';
 import express from 'express';
 import expressSession from 'express-session';
+import bcrypt from 'bcrypt';
+import db from '../lib/database';
 
 const router = express.Router();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
 
 router.get('/', (req, res, next) => {
   res.render('login');
@@ -22,26 +24,46 @@ router.post('/submit', (req, res, next) => {
     password: req.body.password,
   };
 
-  connection.query(
+  // db.query(
+  //   'SELECT * FROM users WHERE username = ?',
+  //   username,
+  //   (error, results, fields) => {
+  //     if (error) throw error;
+
+  //     // catch when result is empty
+  //     if (results.length === 0) {
+  //       res.send('WRONG USERNAME OR PASSWORD!');
+  //     } else if (
+  //       results[0].username.toLowerCase() === username.toLowerCase() &&
+  //       results[0].password === password
+  //     ) {
+  //       // connection.end();
+  //       req.session.success = true;
+  //       req.session.username = results[0].username;
+  //       res.redirect('/');
+  //     } else {
+  //       res.send('WRONG USERNAME OR PASSWORD!');
+  //     }
+  //   }
+  // );
+
+  db.query(
     'SELECT * FROM users WHERE username = ?',
     username,
     (error, results, fields) => {
       if (error) throw error;
 
-      // catch when result is empty
-      if (results.length === 0) {
-        res.send('WRONG USERNAME OR PASSWORD!');
-      } else if (
-        results[0].username.toLowerCase() === username.toLowerCase() &&
-        results[0].password === password
-      ) {
-        // connection.end();
-        req.session.success = true;
-        req.session.username = results[0].username;
-        res.redirect('/');
-      } else {
-        res.send('WRONG USERNAME OR PASSWORD!');
-      }
+      if (results.length === 0) res.send('WRONG USERNAME OR PASSWORD!');
+
+      if (results[0].username.toLowerCase() !== username.toLowerCase())
+        res.send('WRONG USERNAME!');
+
+      bcrypt.compare(password, results[0].password, (err, response) => {
+        if (response) {
+          req.session.success = true;
+          req.session.username = results[0].username;
+        } else res.send('WRONG PASSWORD!');
+      });
     }
   );
 });
