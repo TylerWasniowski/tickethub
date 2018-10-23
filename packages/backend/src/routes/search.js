adc6a4b2e41ebbc679aa7dc4ee4eafa232a51232
TylerWasniowski <TylerWasniowski@gmail.com>
2018-10-22 18:21:51 -0700

    Changed output to list all events who contain substrings of the query

--- packages/backend/src/routes/search.js
+++ packages/backend/src/routes/search.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.get('/suggestions/:query', (req, res, next) => {
   db.query(
-    `SELECT Name FROM events WHERE Name LIKE${db.escape(
-      `${req.params.query}%`
+    `SELECT Name FROM events WHERE Name LIKE ${db.escape(
+      `%${req.params.query}%`
     )}`,
     (error, results, fields) => {
       if (error) throw error;
