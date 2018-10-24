dd8b4818f11e5b1e74f1ed8cff2ac0e2c0527dfb
Zan Javed <zjaved101@gmail.com>
2018-10-24 15:27:53 -0700

    cleaned up code

--- packages/backend/src/routes/account.js
+++ packages/backend/src/routes/account.js
@@ -5,18 +5,6 @@ import db from '../lib/database';
 const router = express.Router();
 
 router.post('/create-account/submit', (req, res, next) => {
-  // const ret = {
-  //   name: req.body.name,
-  //   username: req.body.username,
-  //   email: req.body.email,
-  //   password: req.body.password,
-  // };
-
-  // db.query('INSERT INTO users SET ?', ret, (error, results, fields) => {
-  //   if (error) throw error;
-  //   res.redirect('/');
-  // });
-
   bcrypt.hash(
     req.body.password,
     parseInt(process.env.BCRYPT_SALT, 10),
@@ -75,37 +63,12 @@ router.post('/login/submit', (req, res, next) => {
 
   console.log(username + password);
 
-  // db.query(
-  //   'SELECT * FROM users WHERE username = ?',
-  //   [username],
-  //   (error, results, fields) => {
-  //     if (error) throw error;
-
-  //     // catch when result is empty
-  //     if (results.length === 0) {
-  //       res.send('WRONG USERNAME OR PASSWORD!');
-  //     } else if (
-  //       results[0].username.toLowerCase() === username.toLowerCase() &&
-  //       results[0].password === password
-  //     ) {
-  //       // connection.end();
-  //       req.session.success = true;
-  //       req.session.username = results[0].username;
-  //       res.redirect('/');
-  //     } else {
-  //       res.send('WRONG USERNAME OR PASSWORD!');
-  //     }
-  //   }
-  // );
-
   db.query(
     'SELECT * FROM users WHERE username = ?',
     username,
     (error, results, fields) => {
       if (error) throw error;
 
-      // if (results.length === 0)
-      //   res.json('WRONG USERNAME OR PASSWORD!');
       if (
         results.length === 0 ||
         results[0].username.toLowerCase() !== username.toLowerCase()
@@ -113,11 +76,9 @@ router.post('/login/submit', (req, res, next) => {
         res.json('WRONG USERNAME!');
       else {
         bcrypt.compare(password, results[0].password, (err, response) => {
-          // res.json({pass: typeof password, hash: typeof results[0].password});
           if (err) throw err;
 
           if (response) {
-            // res.json('inside');
             req.session.success = true;
             req.session.username = results[0].username;
             res.json('LOGIN SUCCESSFUL');
