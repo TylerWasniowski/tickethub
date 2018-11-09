05d7de166eea7dcf474385cd886157531e0f8ca2
Justin Ocampo <lingling287@gmail.com>
2018-11-09 13:35:24 -0800

    check if credit card exist in db

--- packages/backend/src/lib/creditcard.js
+++ packages/backend/src/lib/creditcard.js
@@ -1,4 +1,3 @@
-import mysql from 'mysql';
 import { dbQueryPromise } from './database';
 
 // returns true or false if the credit card number is a valid credit card
@@ -10,17 +9,16 @@ export function validCreditCard(input) {
   let nCheck = 0;
   let nDigit = 0;
   let bEven = false;
-  const number = input.replace(/\D/g, '');
+  const value = input.replace(/\D/g, '');
 
-  for (let n = number.length - 1; n >= 0; n -= 1) {
-    const cDigit = number.charAt(n);
-
+  for (let n = value.length - 1; n >= 0; n -= 1) {
+    const cDigit = value.charAt(n);
     nDigit = parseInt(cDigit, 10);
 
     if (bEven) {
       nDigit *= 2;
       if (nDigit > 9) {
-        nDigit = -9;
+        nDigit -= 9;
       }
     }
 
@@ -31,6 +29,17 @@ export function validCreditCard(input) {
   return nCheck % 10 === 0;
 }
 
-export function checkCreditCard() {
-  return false;
+export async function checkCreditCard(number, name, cvv, exp) {
+  dbQueryPromise(
+    `SELECT * 
+     FROM credit_cards
+     WHERE number=? AND name=? AND cvv=? AND experation=?`,
+    [number, name, cvv, exp]
+  )
+    .then(results => {
+      if (!results) return 'Invalid Credit Card Info';
+
+      return results[0].name;
+    })
+    .catch(console.log('Error connecting to db'));
 }
--- packages/backend/src/routes/index.js
+++ packages/backend/src/routes/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
-import db from '../lib/database';
+
+import { checkCreditCard, validCreditCard } from '../lib/creditcard';
 
 const router = express.Router();
+
+router.get('/test/creditcard/:input', (req, res, next) => {
+  res.send(validCreditCard(req.params.input));
+});
+
+router.post('/check/creditcard/', (req, res, next) => {
+  console.log(req.body.number + req.body.name + req.body.cvv + req.body.exp);
+
+  res.send(
+    checkCreditCard(req.body.number, req.body.name, req.body.cvv, req.body.exp)
+  );
+});
 
 export default router;
