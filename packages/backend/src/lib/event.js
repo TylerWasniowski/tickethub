9400d63eda448ef134328cba424172338c37fc98
Zan Javed <zjaved101@gmail.com>
2018-11-17 22:32:04 -0800

    added event functions for creation and retrieval

--- packages/backend/src/lib/event.js
+++ packages/backend/src/lib/event.js
@@ -11,13 +11,15 @@ export async function checkEvents(eventName) {
   Checks if event already exists in the events table in database
   param eventName string: the name of event
   */
-  return dbQueryPromise('SELECT name FROM events WHERE name = ?', eventName)
+  return dbQueryPromise('SELECT name FROM events WHERE name = ?', [eventName])
     .then(results => {
-      if (!results.length) return false;
-
+      if (!results.length) {
+        console.log('NOT FOUND');
+        return false;
+      }
       return true;
     })
-    .catch(console.log);
+    .catch(err => console.log(`ERROR: ${JSON.stringify(err)}`));
 }
 
 export async function createEvent(
@@ -25,19 +27,40 @@ export async function createEvent(
   eventDateTime = null,
   eventVenue = null,
   eventCity = null,
-  eventDetails = null
+  eventDetails = null,
+  eventArtistName = null,
+  eventImage = null
 ) {
   /*
   Creates an event in the event table if it does not already exist
   */
-  if (!checkEvents(eventName)) {
-    return dbQueryPromise('INSERT INTO events VALUES(?,?,?,?,?', [
-      eventName,
-      eventDateTime,
-      eventVenue,
-      eventCity,
-      eventDetails,
-    ]).catch(console.log);
+  if (!(await checkEvents(eventName))) {
+    return dbQueryPromise(
+      'INSERT INTO events (name,dateTime,venue,city,details,artistName,image) VALUES(?,?,?,?,?,?,?)',
+      [
+        eventName,
+        eventDateTime,
+        eventVenue,
+        eventCity,
+        eventDetails,
+        eventArtistName,
+        eventImage,
+      ]
+    ).catch(err => console.log(`ERROR: ${JSON.stringify(err)}`));
   }
   return false;
+}
+
+export async function getEventId(eventName, eventDateTime) {
+  return dbQueryPromise(
+    'SELECT id FROM events WHERE name = ? AND dateTime = ?',
+    [eventName, eventDateTime]
+  )
+    .then(results => {
+      if (!results.length) {
+        return false;
+      }
+      return results[0].id;
+    })
+    .catch(err => console.log(`ERROR: ${JSON.stringify(err)}`));
 }
