2093c6979148be787f1a5edacda09f40dbbf39f6
verawong789 <33075493+verawong789@users.noreply.github.com>
2018-11-18 17:37:30 -0800

    Upload ticket (#153)
    * added checkOut
    
    * added check out page
    
    * fixing bugs
    
    * Created icon
    
    * Separated Search from Home
    
    * Merged with master
    
    * first commit
    
    * sell ticket
    
    * submit new ticket, bought ticket
    
    * Moved account pages to account icon
    
    *  incomplete
    
    * merge master
    
    * Implemented ticket locking
    
    * more fixes in ticket
    
    * more fixes in ticket
    
    * fixed new event
    
    * fixed new event
    
    * Added ticket loading wheel
    
    * fixed account and ticket
    
    * fixed db
    
    * starting checkout transaction
    
    * credit card functions and checkout transactions incomplete
    
    * more checkout
    
    * checkout transaction
    
    * cleaning up comments
    
    * took out cardExists function
    
    * moved distance calculation part
    
    * removed testing routes from index
    
    * changed billing/submit route to just /submit
    
    * removed debug from front end index
    
    * check if card exist for user is working now
    
    * fixed case of multiple res.send in checkout route
    
    * added http error responses
    
    * added log in checks to all routes that require user to be logged in in order to be performed
    
    * fix distance calculation
    
    * fix distance calculation
    
    * changes in new ticket and new event
    
    * combined transaction, changed sale-charge
    
    * changed status codes
    
    * made requested changes in ticket,lib, account
    
    * incomplete changes, added param
    
    * fixed checkout after merging with master
    
    * fixed sell ticket url route for front end

--- packages/backend/src/routes/ticket.js
+++ packages/backend/src/routes/ticket.js
@@ -1,11 +1,15 @@
 import express from 'express';
-import { db } from '../lib/database';
+import status from 'http-status';
+import { db, dbQueryPromise } from '../lib/database';
 import {
   getAssignedTicket,
   lockTicket,
   hasTicket,
+  getTicketInfo,
   getTicket,
 } from '../lib/tickets';
+import { cardExists } from '../lib/creditcard';
+import { getDistance } from '../lib/distanceMatrix';
 
 const router = express.Router();
 
@@ -15,32 +19,79 @@ router.get('/:id', (req, res, next) => {
   return getTicket(req.params.id);
 });
 
+// idea: choose from existing events or create new event
+// new event
+router.post('/new-event/submit', async (req, res, next) => {
+  const eventInfo = {
+    name: req.body.name,
+    dateTime: req.body.dateTime,
+    venue: req.body.venue,
+    city: req.body.city,
+    details: req.body.details, // can be null
+    artistName: req.body.artistName,
+  };
+
+  if (req.session.userId == null) {
+    res.status(status.NOT_ACCEPTABLE).json('Not logged in');
+  } else {
+    dbQueryPromise(
+      'INSERT INTO events (name, dateTime, venue, city, details, artistName) VALUES (?,?,?,?,?,?)',
+      [
+        eventInfo.name,
+        eventInfo.dateTime,
+        eventInfo.venue,
+        eventInfo.city,
+        eventInfo.details,
+        eventInfo.artistName,
+      ]
+    ).catch(err =>
+      console.log(`Error contacting database: ${JSON.stringify(err)}`)
+    );
+    res.status(status.OK).json();
+  }
+});
+
 // new ticket
-router.post('/sell/submit', (req, res, next) => {
+router.post('/sell/submit', async (req, res, next) => {
   const ticketInfo = {
-    eventId: req.body.eventId,
     price: req.body.price,
+    eventId: req.body.eventId,
     seat: req.body.seat, // can be null, general seating
   };
 
-  db.query(
-    'INSERT INTO tickets (sellerId, eventId, price, seat) VALUES (?,?,?,?)',
-    [req.session.userId, ticketInfo.eventId, ticketInfo.price, ticketInfo.seat],
-    (error, results, fields) => {
-      if (error) {
-        console.log(`Error contacting database: ${JSON.stringify(error)}`);
-        res.json(500, error);
-      }
-
-      res.json('OK');
-    }
-  );
+  if (req.session.userId == null) {
+    res.status(status.NOT_ACCEPTABLE).json('Not logged in');
+  } else if ((await cardExists(req.session.userId)) === false) {
+    res
+      .status(status.NOT_ACCEPTABLE)
+      .send('User does not have a credit card on file');
+  } else {
+    dbQueryPromise(
+      'INSERT INTO tickets (sellerId, eventID, price, seat) VALUES (?,?,?,?)',
+      [
+        req.session.userId,
+        ticketInfo.eventId,
+        ticketInfo.price,
+        ticketInfo.seat,
+      ]
+    ).catch(err =>
+      console.log(`Error contacting database: ${JSON.stringify(err)}`)
+    );
+    res.status(status.OK).json();
+  }
 });
 
-router.get('/sale-charge/:id', async (req, res, next) => {
+router.get('/sale-charge/:id/:deliveryMethod', async (req, res, next) => {
   const ticket = await getTicket(req.params.id);
 
-  const ret = ticket.price * 1.05;
+  const distance = await getDistance(req.params.id, req.session.userId); // res.json(`The distance is: ${distance}`);
+
+  const ret = {
+    price: ticket.price,
+    fivePercent: ticket.price * 0.05,
+    shipping: req.params.deliveryMethod, // NEEDS METHOD, use distance
+  };
+
   res.json(ret);
 });
 
